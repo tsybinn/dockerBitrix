ae8849ee81424f288746eafcd0cda514b4d4b461
tsybin <tsybin@samsonpost.ru>
2021-03-21 16:58:28 +0300

    вынес www,

--- apache/start.sh
+++ apache/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-cd  /var/www/html/
-wget -c https://dev.1c-bitrix.ru/download/scripts/bitrix_server_test.php
-wget -c https://www.1c-bitrix.ru/download/scripts/bitrixsetup.php
-touch index.php
-echo '<? phpinfo();mail("test@ryu.ru","test","test");' > index.php
+#cd  /var/www/html/
+#wget -c https://dev.1c-bitrix.ru/download/scripts/bitrix_server_test.php
+#wget -c https://www.1c-bitrix.ru/download/scripts/bitrixsetup.php
+#touch index.php
+#echo '<? phpinfo();mail("test@ryu.ru","test","test");' > index.php
 chown -R www-data:www-data  /var/www/html
 apache2-foreground
